8a7eef64b055466fb09c0f43ac815fd96bce4dae
senzoadmin <shanchezwiggins3@gmail.com>
2025-03-14 10:31:19 -0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,8 @@
+local GameID = game.GameId
+local Player = game:GetService("Players").LocalPlayer
+
+if GameID ~= 4483381587 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/senzoadmin/senzohubff2/refs/heads/main/featherlite.lua"))() -- FF2
+else
+    Player:Kick("Feather | Game currently unsupported. Check the Status Channel for supported games.")
+end
