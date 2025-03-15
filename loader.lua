bbdbb1b1ca82df59a82e7413fe71bafd815d598b
senzoadmin <shanchezwiggins3@gmail.com>
2025-03-15 10:46:29 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,7 +2,7 @@ local GameID = game.GameId
 local Player = game:GetService("Players").LocalPlayer
 
 if GameID ~= 8204899140 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/senzoadmin/featherlite/refs/heads/main/featherlite.lua"))() -- FF2
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/senzoadmin/featherlite/refs/heads/main/ff2script.lua"))() -- FF2
 else
 if GameID ~= 2338325648 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/senzoadmin/featherlite/refs/heads/main/nfluniverse"))() -- NFL
