b7ecfafda78d0132d4f2b31b29ec99fab9d81e37
senzoadmin <shanchezwiggins3@gmail.com>
2025-03-15 10:47:03 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,7 +5,7 @@ if GameID ~= 8204899140 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/senzoadmin/featherlite/refs/heads/main/ff2script.lua"))() -- FF2
 else
 if GameID ~= 2338325648 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/senzoadmin/featherlite/refs/heads/main/nfluniverse"))() -- NFL
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/senzoadmin/featherlite/refs/heads/main/nfluniverse.lua"))() -- NFL
 else
     Player:Kick("Feather | Game currently unsupported. Check the Status Channel for supported games.")
 end
