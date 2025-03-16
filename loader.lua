466c130965b4bfd47ea8bff7b6ae34387a415485
senzoadmin <shanchezwiggins3@gmail.com>
2025-03-16 07:45:57 -0400

    added blr

--- loader.lua
+++ loader.lua
@@ -7,5 +7,8 @@ else
 if GameID ~= 2338325648 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/senzoadmin/featherlite/refs/heads/main/nfluniverse.lua"))() -- NFL
 else
+if GameID ~= 18668065416 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/senzoadmin/featherlite/refs/heads/main/bluelockr.lua"))() -- NFL
+else
     Player:Kick("Feather | Game currently unsupported. Check the Status Channel for supported games.")
 end
